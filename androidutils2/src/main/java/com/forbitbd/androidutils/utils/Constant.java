0c7d5be42952a416f28d88f4f6e2d0efcb1f650e
sohel2178 <sohel.ahmed2178@gmail.com>
2019-11-22 00:30:51 +0600

    11/22/2019 at 12:30 PM

--- androidutils2/src/main/java/com/forbitbd/androidutils/utils/Constant.java
+++ androidutils2/src/main/java/com/forbitbd/androidutils/utils/Constant.java
@@ -6,4 +6,21 @@ public class Constant {
     public static final String TITLE = "TITLE";
     public static final String TIME = "TIME";
     public static final String CONTENT = "CONTENT";
+
+    public static final String PROJECT="PROJECT";
+    public static final String WORKER="WORKER";
+    public static final String WORKER_LIST="WORKER_LIST";
+    public static final String ATTENDANCE_RESPONSE="ATTENDANCE_RESPONSE";
+    public static final String EMPLOYEE="EMPLOYEE";
+    public static final String STATUS="STATUS";
+    public static final String UPDATE="UPDATE";
+    public static final String DELETE="DELETE";
+    public static final String TASK="TASK";
+    public static final String TASK_LIST="TASK_LIST";
+    public static final String UNITS="UNITS";
+    public static final String ACCOUNT="ACCOUNT";
+    public static final String ACCOUNT_LIST="ACCOUNT_LIST";
+    public static final String TRANSACTION="TRANSACTION";
+    public static final String POSITION="POSITION";
+    public static final String SUPPLIER="SUPPLIER";
 }
